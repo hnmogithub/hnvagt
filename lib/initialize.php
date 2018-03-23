f56539d424438b82d1470818130ce4588fec6f22
istasi <istasi@gmail.com>
2018-03-23 15:29:23 +0100

    ...

--- lib/initialize.php
+++ lib/initialize.php
@@ -4,5 +4,19 @@ spl_autoload_register ( function ( $class )
     if ( file_exists ( 'lib/'. $class .'.php' ) == true  )
     {
         require_once ( 'lib/'. $class .'.php' );
+
+        return;
+    }
+
+    $mod = schedule::lastModule ();
+    if ( $mod !== 'anonymouse' )
+    {
+        $mod = str_replace ( '\\', '/', $mod ) .'/'. $class .'.php';
+        if ( file_exists ( $mod ) == true )
+        {
+            require_once ( $mod );
+
+            return;
+        }
     }
 } );
