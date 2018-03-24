032dbb291e6d8c6d1c1826848d3261e1325bc4b8
istasi <istasi@gmail.com>
2018-03-24 19:54:28 +0100

    ...

--- modules/login/login.php
+++ modules/login/login.php
@@ -6,11 +6,18 @@ class login
 {
 	public function __construct ()
 	{
-		schedule::add ( schedule::$RUN_INIT, [ $this, 'check' ] );
+		schedule::add ( schedule::$RUN_INIT, [ $this, 'init' ], ['url'] );
+		schedule::add ( schedule::$RUN_MIDDLE, [ $this, 'run' ] );
+	}
+
+	public function init ( $url )
+	{
+		$url->alias ( '/login/login.css', '/system/login/login.css' );
 	}
 
 	public function check ()
 	{
+		template::addCSS ('/login/login.css');
 		template::add ('login.twig', [] );
 	}
 }
