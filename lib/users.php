d328a9eafcde8c563917c2de091132424a06b061
istasi <istasi@gmail.com>
2018-03-25 00:51:41 +0100

    ...

--- lib/users.php
+++ lib/users.php
@@ -96,8 +96,8 @@ class users
 		if ( $conn == false )
 		{	throw new Response ('Unable to connect to ldap', 400); }
 
-		if ( ldap_start_tls ( $conn ) == false )
-		{	throw new Response ('Unable to start TLS on ldap', 400); }
+		//if ( ldap_start_tls ( $conn ) == false )
+		//{	throw new Response ('Unable to start TLS on ldap', 400); }
 
 		if ( ldap_bind ( $conn, $username, $password ) == false )
 		{	throw new Response ('Unable to bind ldap', 400); }
