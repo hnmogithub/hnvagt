bb85e4887561e1dec935434d1922f9f7e041971e
istasi <istasi@gmail.com>
2018-03-25 18:22:28 +0200

    ...

--- system/loader/loader.php
+++ system/loader/loader.php
@@ -68,6 +68,7 @@ class loader
 				? LIKE CONCAT(`mu`.`url`, "%")
 		', [ $id, $id, $_SERVER ['REQUEST_URI'] ] )->each ( function ( $row )
 		{
+			echo $row ['path'];
 			schedule::load ( $row ['path'], $row ['namespace'] );
 		} );
 	}
