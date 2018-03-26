8c4359b63a41807ffd87bdb815f7e04d5af14fd3
istasi <istasi@gmail.com>
2018-03-26 16:06:06 +0200

    ...

--- modules/register/register.php
+++ modules/register/register.php
@@ -33,9 +33,6 @@ class register
 		{	$_SESSION ['register_filter'] = [ user::current () ]; }
 		$filter =& $_SESSION ['register_filter'];
 
-		$reports = reports::byUsers ( $filter );
-		var_dump ( $reports [0] );
-
 		template::add ('web/index.twig', [
 			'reports' => reports::byUsers ( $filter ),
 			'filter' => $filter
