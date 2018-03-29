3a8877e9e08c34ac6fb7379bc37bcd6c50a552db
istasi <istasi@gmail.com>
2018-03-29 19:27:44 +0200

    ...

--- modules/register_new/register_new.php
+++ modules/register_new/register_new.php
@@ -13,14 +13,23 @@ class register_new
 		schedule::add ( schedule::$RUN_INIT, [ $this, 'init' ], [ 'url' ] );
 	}
 
+	/**
+	 * Bind the right url we need
+	 */
 	public function init ( $url )
 	{
+		$url->request ( '/register/new/ajax', schedule::$RUN_INIT, [ $this, 'ajax' ] );
 		$url->request ( '/register/new/', schedule::$RUN_MIDDLE, [ $this, 'run' ] );
 	}
 
+	/**
+	 * Add the html stuff we need
+	 */
 	public function run ()
 	{
 		template::addCSS ( 'web/index.css' );
+		template::addCSS ( 'web/lib/anytime/anytime.5.2.0.css' );
+
 		template::add ( 'web/index.twig', [ 'user' => user::current () ] );
 	}
 }
--- modules/register_new/web/index.js
+++ modules/register_new/web/index.js
@@ -2,6 +2,6 @@ r ( function ()
 {
 	$.getScript ('/modules/register_new/web/lib/anytime/anytime.5.2.0.min.js', function ()
 	{
-		
+		$('input.datetime').AnyTime_picker ();
 	} );
 } );
