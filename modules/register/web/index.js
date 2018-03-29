7a5f65b138b3d019176e59ae2cf2367166aaf39b
istasi <istasi@gmail.com>
2018-03-29 21:25:43 +0200

    ...

--- modules/register/web/index.js
+++ modules/register/web/index.js
@@ -6,7 +6,7 @@ r ( function ()
 		switch ( e.which )
 		{
 			case 78: // N
-				return $('#register-new-task').trigger ('click');
+				return $('#register-new-task').get(0).click ();
 		}
 	} );
 } );
