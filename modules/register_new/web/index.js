f4aeb139bd4eee5343fcec9a972bbc4f779d14ac
istasi <istasi@gmail.com>
2018-03-29 19:53:18 +0200

    ...

--- modules/register_new/web/index.js
+++ modules/register_new/web/index.js
@@ -3,7 +3,7 @@ r ( function ()
 	$.getScript ('/modules/register_new/web/lib/anytime/anytime.5.2.0.min.js', function ()
 	{
 		AnyTime.picker ( 'date-from', {
-			'format': '%Y-%m-%d %T'
+			'format': '%Y-%m-%d %H:%i'
 		} );
 	} );
 } );
