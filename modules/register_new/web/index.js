1d7b1da31b0a2b1d4f46e6a2e751398f41e50a99
istasi <istasi@gmail.com>
2018-03-29 19:46:47 +0200

    ...

--- modules/register_new/web/index.js
+++ modules/register_new/web/index.js
@@ -2,12 +2,8 @@ r ( function ()
 {
 	$.getScript ('/modules/register_new/web/lib/anytime/anytime.5.2.0.min.js', function ()
 	{
-		$('input.datetime').on ( 'click', function ()
-		{
-			$(this).off ('click').AnyTime_picker ().focus ();
-		} ).on ('blur', function ()
-		{
-			$(this).AnyTime_noPicker ();
+		AnyTime.picker ( 'date-from', {
+			'format': '%Y-%m-%d %T'
 		} );
 	} );
 } );
