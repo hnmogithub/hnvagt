82465772e7cef9fec3a80b5519dc0c6dbbb3b2d1
istasi <istasi@gmail.com>
2018-03-29 15:53:56 +0200

    ...

---
+++ modules/register_new/register_new.php
@@ -0,0 +1,25 @@
+<?php namespace modules;
+use \schedule;
+use \template;
+use \user;
+
+/**
+ * Module for handling the creation of entries into the register
+ */
+class register_new
+{
+	public function __construct ()
+	{
+		schedule::add ( schedule::$RUN_INIT, [ $this, 'init' ], [ 'url' ] );
+	}
+
+	public function init ( $url )
+	{
+		$url->request ( '/register/new/', schedule::$RUN_MIDDLE, [ $this, 'run' ] );
+	}
+
+	public function run ()
+	{
+		template::add ( 'web/index.twig', [ 'user' => user::current () ] );
+	}
+}
