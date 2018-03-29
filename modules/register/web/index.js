0fb54c22902add440b844d2465d5826ddfec4b1a
istasi <istasi@gmail.com>
2018-03-29 21:28:57 +0200

    ...

--- modules/register/web/index.js
+++ modules/register/web/index.js
@@ -2,10 +2,13 @@ r ( function ()
 {
 	$(document).on ('keyup', function ( e )
 	{
-		console.log ( e.which, e.keyCode );
+		if ( window.location.pathname == '/register/new/' )
+		{	return; }
+
 		switch ( e.which )
 		{
 			case 78: // N
+				if ( $('*:focus').is ('input') == true ) { return; }
 				return $('#register-new-task').get(0).click ();
 		}
 	} );
