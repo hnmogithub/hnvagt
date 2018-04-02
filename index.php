13e4c35175d55a1be9bae05dc93ae11a8d915a68
istasi <istasi@gmail.com>
2018-04-02 13:30:54 +0200

    ...

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php 
 error_reporting ( E_ALL );
 ini_set("display_errors", 1);
-date_default_timezone_set('Europe/Copenhagen'); // Setting php.ini up is hard
+//date_default_timezone_set('Europe/Copenhagen'); // Setting php.ini up is hard
 
 include ( 'lib/initialize.php' );
 
